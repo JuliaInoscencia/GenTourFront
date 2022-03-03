fb213a44ecbb00584c9b34e2295cb8d809675c86
Darllan <darllan13@hotmail.com>
2022-03-03 16:15:14 -0300

    @Darllan1L [ Task: edit service]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -18,4 +19,13 @@ export class AuthService {
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>("https://gentour.herokuapp.com/usuario/cadastrar", usuario)
   }
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token != '') {
+      ok = true
+    }
+    return ok
+  }
 }
+
